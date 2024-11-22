c05525b0282bb494e59f7425ceb9b1ec7cd2f237
owlf1x0r <owlf1x0r@gmail.com>
2024-11-22 11:23:52 +0900

    배경색 primary로 변경

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-background">
+    <div className="flex items-center justify-center min-h-screen bg-primary">
       <PomodoroTimer sessionRecords={sessionRecords} updateSessionRecord={updateSessionRecord} />
     </div>
   );
